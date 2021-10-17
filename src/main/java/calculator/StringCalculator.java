ca19f8efb32e4a9092fe67ebc5313510f0d4cec1
bhavna wankhede <bhavnawankhede@gmail.com>
2021-10-17 14:52:27 +0530

    Commited 1,2,3,4,5 in branch1

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,56 @@
 package calculator;
 
 class StringCalculator {
-
-    public int add(String input) {
-        return 0;
+	public int add(String input) {
+    	String numbers[]=getSplit(input);
+    	if (input.isEmpty()) {
+            return 0;
+        } 
+    	else if(numbers.length>1) {
+    		return getSum(numbers);
+    	}
+    	 return stringToInt(input);
     }
+
+	
+	private int getSum(String[] numbers) {
+		 int sum = 0;
+	        for (String currentNumber:numbers) {
+	           // if (stringToInt(currentNumber) > 1000) {
+	                //continue;
+	           // }
+	            sum += stringToInt(currentNumber) ;
+	        }
+	 return sum;
+	}
+	
+	private String[] getSplit(String string) {
+		if(string.startsWith("//")) {
+			Matcher matcher=Pattern.compile("//(.)\n(.*)").matcher(string);
+			Matcher matcher1=Pattern.compile("//(...)\n(...*)").matcher(string);
+			if(matcher.matches()) {
+				String delimiter=matcher.group(1);
+				String toSplit=matcher.group(2);
+				return toSplit.split(delimiter);
+			}
+			
+			if(matcher1.matches()) {
+				String delimiter=matcher1.group(1);
+				String toSplit=matcher1.group(2);
+				return toSplit.split(delimiter);
+			}
+		}
+		return string.split(",|\n");
+	}
+	
+	private int stringToInt(String input) {
+		int num = Integer.parseInt(input);
+        if (num < 0) {
+           throw new IllegalArgumentException("Negatives not allowed");
+        	
+        } else {
+            return num;
+        }
+	}
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,15 +6,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+    StringCalculator stringCalculator = new StringCalculator();
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+
+
+	   @Test
+	    void empty_string_should_return_0() {
+	        assertEquals(0, stringCalculator.add(""));
+	    }
+
+	    @Test
+	    void string_with_single_number_should_return_number_as_int() {
+	        assertEquals(1, stringCalculator.add("1"));
+	    }
+	    
+	    @Test
+	    void string_with_two_numbers_should_return_number_as_int() {
+	        assertEquals(3, stringCalculator.add("1,2"));
+	    }
+	    
+	    @Test
+	    void string_with_multiple_numbers_should_return_number_as_int() {
+	        assertEquals(55, stringCalculator.add("1,2,3,4,5,6,7,8,9,10"));
+	    }
+	    
+	    @Test
+	    void string_with_new_line_and_numbers_should_return_number_as_int() {
+	       assertEquals(55, stringCalculator.add("1\n2,3,4,5,6,7,8,9,10"));
+	    }
+	    
+	    @Test
+	    void string_with_different_delimiters_should_return_number_as_int() {
+	       assertEquals(6, stringCalculator.add("//;\n1;2;3"));
+	    }
+	    
+	   @Test
+	    void string_with_negative_input_throw_Exception() {
+	      assertEquals(stringCalculator.add("-1,9,-8"),"Negatives not allowed");
+	    }
 }
